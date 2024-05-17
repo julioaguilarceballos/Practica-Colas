838ef7f244c1993b1bacc804291a0538cc034d38
julioaguilarceballos <juliocesaraguilarceballos13@gmail.com>
2024-05-17 10:03:19 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Practica_Colas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear Colas
+            Queue miFila = new Queue();
+            Console.Write("Ingrese la Cantidad de filas: ");
+            int n = int.Parse(Console.ReadLine());
+            int tamañoFila = n;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nombre: ");
+                miFila.Enqueue(Console.ReadLine());
+
+            }
+            Console.WriteLine("<----------------------------------------------------->");
+            Console.WriteLine("El cajero atendio a: ");
+            while (tamañoFila > 0)
+            {
+                Console.Write("[" + miFila.Dequeue() + "]");
+                tamañoFila--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
